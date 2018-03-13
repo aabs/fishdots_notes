7f8dcae3f0c59ee0aa1353f463cf57f0095f69fb
Andrew Matthews <matthews.andrew@gmail.com>
2018-03-14 05:29:29 +1100

    creations

--- init/rc1.d/01-functions.fish
+++ init/rc1.d/01-functions.fish
@@ -7,16 +7,23 @@
 # rename note
 
 function note_create -a title -d "create a new text note"
-  set -l escaped_file_name (_escape_file_name $title)
-  set -l p $FD_NOTES_HOME/(date --iso-8601)
+  set escaped_file_name (_escape_string $title)
+  set d (date --iso-8601)
+  set p "$FD_NOTES_HOME/$d-$escaped_file_name.md"
+  touch $p
+  note_edit $p
 end
 
-function note_create_project_note -d "create a new text note within a project area"
-  
-end
-
-function _note_find -a pattern -d "find note by note name"
-    find $FD_NOTES_HOME/ -iname "*$pattern*"
+function note_create_project_note  -a title -d "create a new text note within a project area"
+  if not set -q $CURRENT_PROJECT_SN
+    note_create $title
+    return
+  end
+  set escaped_file_name (_escape_string $title)
+  set d (date --iso-8601)
+  set p "$FD_NOTES_HOME/$CURRENT_PROJECT_SN/$d-$escaped_file_name.md"
+  touch $p
+  note_edit $p
 end
 
 function _note_search -a pattern -d "find note by full text search"
@@ -37,11 +44,26 @@ function note_edit -a file_path -d "open the file in vim"
   end
 end
 
-function _escape_file_name -a title -d "remove non-path characters"
-  echo (string replace ' ' '_')  
+function _escape_string -a title -d "remove non-path characters"
+  set r $title
+  set replacements_performed 1 # set to 1 initially to get into the loops
+  while test $replacements_performed > 0
+    set replacements_performed 0
+    for c in " " ":" "/" "\\" "\t" "\n" ";" "." "," "__"
+      set r (string replace $c '_' $r)
+      if test $status -eq 0
+        set replacements_performed (math $replacements_performed + 1)
+      end
+    end
+    # if no replacements were possible then stop
+    if test $replacements_performed -eq 0
+      break
+    end
+  end  
+  echo $r
 end
 
-function note_find -a search_pattern
+function note_find -a search_pattern -d "file name search for <pattern>, opens selection in default editor"
   set matches (_note_find "$search_pattern")
   set -g dcmd "dialog --stdout --no-tags --menu 'select the file to edit' 20 60 20 " 
   set c 1
@@ -57,7 +79,7 @@ function note_find -a search_pattern
   end
 end
 
-function note_search -a search_pattern
+function note_search -a search_pattern -d "full text search for <pattern>, opens selection in default editor"
   set matches (_note_search "$search_pattern")
   set -g dcmd "dialog --stdout --no-tags --menu 'select the file to edit' 20 60 20 " 
   set c 1
@@ -77,3 +99,8 @@ function get_file_relative_path -a path_to_note -d "description"
   set filename (string replace $FD_NOTES_HOME "" $path_to_note)
   echo $filename
 end
+
+function _note_find -a pattern -d "find note by note name"
+    find $FD_NOTES_HOME/ -iname "*$pattern*"
+end
+
