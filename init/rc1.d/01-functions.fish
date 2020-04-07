e2bc65cf5d785cf039c10d3108021e9f7ee66fe0
Andrew Matthews <matthews.andrew@gmail.com>
2020-04-07 22:46:12 +1000

    conversion to new command fw WIP

--- init/rc1.d/01-functions.fish
+++ init/rc1.d/01-functions.fish
@@ -1,35 +1,17 @@
-function note
-  if test 0 -eq (count $argv)
-    note_help
-    return
-  end
-  switch $argv[1]
-    case home
-      cd $FD_NOTES_HOME
-    case ls
-      _note_find '*.md'
-    case tasks
-      note_tasks
-    case edit
-      note_edit $argv[2]
-    case find
-      note_find $argv[2]
-    case search
-      note_search $argv[2]
-    case create
-      note_create $argv[2]
-    case pcreate
-      note_create_project_note $argv[2]
-    case save
-      note_save
-    case sync
-      note_sync
-    case move
-      note_move $argv[2..3]
-    case '*'
-      note_help
-  end
-end
+define_command note "A simple plugin for managing a large hierarchy of textual notes"
+
+define_subcommand note home on_note_home "go to the root of the notes collection"
+define_subcommand note info on_note_info "display config details of note plugin"
+define_subcommand_nonevented note ls note_ls "list all notes (maybe long)"
+define_subcommand note tasks on_note_tasks ""
+define_subcommand_nonevented note edit on_note_edit "edit the note identified by the path"
+define_subcommand_nonevented note find on_note_find "find the note by searching file names"
+define_subcommand_nonevented note search on_note_search "perform a full text search for patterns"
+define_subcommand note create on_note_create "create a new note"
+define_subcommand note pcreate on_note_pcreate "create a new note within a project area"
+define_subcommand note save on_note_save "save any new or modified notes locally (to git)"
+define_subcommand note sync on_note_sync "save notes and push to origin"
+define_subcommand note move on_note_move "rename or move the note"
 
 function _validate_args -a argc argv -d "description"
   if test $argc -eq (count $argv)
@@ -43,47 +25,11 @@ function _not_implemented_warning
   warn "This function has not yet been implemented"
 end
 
-function note_help -d "display usage info"
-  echo "Fishdots Notes Usage"
-  echo "===================="
-  echo "note <command> [options] [args]"
-  echo ""
-  echo "note edit pattern"
-  echo "  edit the note identified by the path"
-  echo ""
-  echo "note find pattern"
-  echo "  find the note by searching file names"
-  echo ""
-  echo "note search pattern"
-  echo "  perform a full text search for patterns"
-  echo ""
-  echo "note create title"
-  echo "  create a new note"
-  echo ""
-  echo "note pcreate title"
-  echo "  create a new note within a project area"
-  echo ""
-  echo "note save"
-  echo "  save any new or modified notes locally"
-  echo ""
-  echo "note move"
-  echo "  explain,,,"
-  echo ""
-  echo "note sync"
-  echo "  synchronise notes with origin git repo"
-  echo ""
-  echo "note home"
-  echo "  cd to the notes directory"
-  echo ""
-  echo "note help"
-  echo "  EXPL"
-end
-
-function note_tasks -d "find all tasks"
+function note_tasks -e on_note_tasks  -d "find all tasks"
   ag --markdown -Q -- '- [ ]' $FD_NOTES_HOME 
 end
 
-function note_create -a title -d "create a new text note"
+function note_create -e on_note_create -a title -d "create a new text note"
   set escaped_file_name (_escape_string $title)
   set d (date --iso-8601)
   set p "$FD_NOTES_HOME/$d-$escaped_file_name.md"
@@ -91,7 +37,7 @@ function note_create -a title -d "create a new text note"
   echo wrote "$p" to notes
 end
 
-function note_create_project_note  -a title -d "create a new text note within a project area"
+function note_create_project_note -e on_note_pcreate  -a title -d "create a new text note within a project area"
   set escaped_file_name (_escape_string $title)
   set d (date --iso-8601)
   set p "$FD_NOTES_HOME/$CURRENT_PROJECT_SN/$d-$escaped_file_name.md"
@@ -103,7 +49,7 @@ function _note_search -a pattern -d "find note by full text search"
   fishdots_search $FD_NOTES_HOME $pattern
 end
 
-function note_move -a from_basename to_basename -d "change the name of a note"
+function note_move -e on_note_move -a from_basename to_basename -d "change the name of a note"
   if test -f $FD_NOTES_HOME/$from_basename
     mv $FD_NOTES_HOME/$from_basename $FD_NOTES_HOME/$to_basename  
   else
@@ -117,7 +63,7 @@ function note_move -a from_basename to_basename -d "change the name of a note"
   end
 end
 
-function note_edit -a file_path -d "open the file in nvim"
+function note_edit -e on_note_edit -a file_path -d "open the file in nvim"
   if test $file_path = '-'
     read -l x
     echo "editing $x"
@@ -146,12 +92,16 @@ function _escape_string -a title -d "remove non-path characters"
   echo $r
 end
 
-function note_find -a search_pattern -d "file name search for <pattern>, opens selection in default editor"
+function note_ls -e on_note_ls -d "just list every markdown based note"
+  _note_find '*.md'
+end
+
+function note_find -e on_note_find -a search_pattern -d "file name search for <pattern>, opens selection in default editor"
   fishdots_find_select $FD_NOTES_HOME $search_pattern
   note_edit $fd_selected_file
 end
 
-function note_search -a search_pattern -d "full text search for <pattern>, opens selection in default editor"
+function note_search -e on_note_search -a search_pattern -d "full text search for <pattern>, opens selection in default editor"
   fishdots_search_select $FD_NOTES_HOME $search_pattern
   note_edit $fd_selected_file
 end
@@ -165,6 +115,10 @@ function _note_find -a pattern -d "find note by note name"
     fishdots_find $FD_NOTES_HOME $pattern
 end
 
-function note_sync -d "save all notes to origin repo"
+function note_sync -e on_note_sync -d "save all notes to origin repo"
   fishdots_git_sync $FD_NOTES_HOME "notes updates and additions"
+end
+
+function note_info -e on_note_info -d "display note plugin config info"
+  echo -e "Home Dir:\t\t$FD_NOTES_HOME"
 end
