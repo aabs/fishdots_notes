bb7103219c52409df752cbe9faebd557968e7122
Andrew Matthews <andrew.matthews@team.telstra.com>
2020-05-25 14:36:17 +1000

    rg+fzf+vim=wow

--- init/rc3.d/01-abbr.fish
+++ init/rc3.d/01-abbr.fish
@@ -1,1 +1,8 @@
 abbr --add fdn note home
+
+function search_notes_fuzzy -d "ripgrep and fzf"
+  set -l file (rg --files $FD_NOTES_HOME/**/*.md | fzf)
+  eval "$EDITOR $file"
+end
+
+abbr -a nts search_notes_fuzzy
